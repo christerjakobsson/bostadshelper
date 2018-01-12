d7da0ca4c07e44cb07f2d164f9734b8d71a5fe7d
Christer Jakobsson <chjak@openlibrarysolutions.com>
2018-01-12 23:16:04 +0100

    playing around with stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { 
+  Component, Input
+} from '@angular/core';
+
+import { Values } from './values';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Bostadshelper';
+  public values: Values;    
+  lagfart = 0;
+  pantbrev = 0;
+
+  constructor() {
+    this.values = new Values();
+    console.log(this.values);
+  }
+
+  onChange() {
+    this.lagfart = this.calculateLagfart(this.values.price);
+    this.pantbrev = this.calculatePantbrev(this.values.price, this.values.mortgages);
+  }
+
+  calculateLagfart(price) {
+    let lagfartAdminAvgift = 825;    
+    return (price * 0.015) + lagfartAdminAvgift;
+  }
+
+  public calculatePantbrev(price, mortgages) {    
+    const loan = price * 0.85; // TODO make slider for kontantinsats 
+    let pantbrev = price - mortgages;
+    return (pantbrev * 0.02) + 375;
+  }
 }
---
+++ src/app/values.ts
@@ -0,0 +1,4 @@
+export class Values {
+  price: number = 0;
+  mortgages: number = 0;
+}
